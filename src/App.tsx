e1f4b9fc60365100ecdaa4503c842f1b04304bdd
thestriver <wannabeamumin@gmail.com>
2023-03-09 16:42:52 -0700

    full-typed API and react

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import UserDisplay from './components/UserDisplay'
+import { useQuery } from 'urql'
+import { GetUsersDocument } from './graphql/generated'
+
+const UsersQuery = `query GetUsers {
+  users {
+    name
+    messages {
+      body
+    }
+  }
+}`
+
+function App() {
+  const [results] = useQuery({
+    query: GetUsersDocument
+  })
+
+  return (
+    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+      {
+        results.data?.users.map((user, i) => <UserDisplay user={user} key={i}/>)
+      }
+    </div>
+  )
+}
+
+export default App
